db1c0a554dbead9d1d6c651ce166975663b5c5fa
Matt Kaye <mrkaye97@gmail.com>
2024-06-22 13:04:53 -0400

    arrangement

--- pages/travel.tsx
+++ pages/travel.tsx
@@ -1,6 +1,5 @@
 import { promises as fs } from "fs";
 import path from "path";
-import React from "react";
 import Image from "next/image";
 import { imageRoot } from "@/src/common/images";
 
@@ -10,18 +9,29 @@ export default function TravelPhotos({
   imageFilenames: string[];
 }) {
   return (
-    <div className="flex flex-col justify-center items-center space-y-4 mb-16">
-      {imageFilenames.map((el: string) => (
-        <div key={el} className="rounded-lg overflow-hidden">
-          <Image
-            width={800}
-            height={600}
-            alt={"alt"}
-            src={imageRoot + `travel/${el}`}
-          />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="text-xl font-bold text-seafoam-green my-4">
+        Sometimes I go places. When I do, I like taking pictures. These are some
+        of my favorites.
+      </div>
+      <div className="flex flex-wrap justify-center items-center space-y-4 mb-16">
+        {imageFilenames.map((el: string) => (
+          <div
+            key={el}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-2"
+          >
+            <div className="rounded-lg overflow-hidden">
+              <Image
+                width={600}
+                height={400}
+                alt={el}
+                src={imageRoot + `travel/${el}`}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
